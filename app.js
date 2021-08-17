4167a4fdef16546d383ca8b56b15cee667be622c
madoximxet <walid.bouachir@gmail.com>
2021-08-17 11:18:22 -0400

    Commit 7

--- app.js
+++ app.js
@@ -10,20 +10,18 @@ app.set('view engine', 'ejs');
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {title: 'Home'});
 });
 
-// app.get('/about', (req, res) => {
-//     // res.send('<p>about page</p>');
-//     res.sendfile('./views/about.html', { root: __dirname });
-// });
+app.get('/about', (req, res) => {
+    res.render('about', {title: 'About'});
+});
 
-// // redirects
-// app.get('/about-us', (req, res) => {
-//     res.redirect('about');
-// });
+app.get('/blogs/create', (req, res) => {
+    res.render('create', {title: 'Create a new blog'});
+});
 
-// // 404 not found
-// app.use((req, res) => {
-//     res.sendfile('./views/404.html', { root: __dirname });
-// });
+ // 404 not found
+ app.use((req, res) => {
+    res.status(404).render('404', {title: '404 Error'});
+});
