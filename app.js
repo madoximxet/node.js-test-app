9ac6bc810897c82087cc93957d66185fcfe1bbf3
madoximxet <walid.bouachir@gmail.com>
2021-08-17 10:29:17 -0400

    Commit 5

--- app.js
+++ app.js
@@ -10,21 +10,20 @@ app.set('view engine', 'ejs');
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    // res.send('<p>home page</p>');
-    res.sendfile('./views/index.html', { root: __dirname });
+    res.render('index');
 });
 
-app.get('/about', (req, res) => {
-    // res.send('<p>about page</p>');
-    res.sendfile('./views/about.html', { root: __dirname });
-});
+// app.get('/about', (req, res) => {
+//     // res.send('<p>about page</p>');
+//     res.sendfile('./views/about.html', { root: __dirname });
+// });
 
-// redirects
-app.get('/about-us', (req, res) => {
-    res.redirect('about');
-});
+// // redirects
+// app.get('/about-us', (req, res) => {
+//     res.redirect('about');
+// });
 
-// 404 not found
-app.use((req, res) => {
-    res.sendfile('./views/404.html', { root: __dirname });
-});
+// // 404 not found
+// app.use((req, res) => {
+//     res.sendfile('./views/404.html', { root: __dirname });
+// });
