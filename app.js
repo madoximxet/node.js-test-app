c6e4503bcce36f73c6ba73e92b7b3a0d7074c645
madoximxet <walid.bouachir@gmail.com>
2021-08-18 19:13:32 -0400

    Commit 11

--- app.js
+++ app.js
@@ -52,6 +52,17 @@ app.post('/blogs', (req, res) => {
     })
 })
 
+app.get('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    Blog.findById(id)
+    .then(result => {
+        res.render('details', { blog: result, title: 'Blog details' });
+    })
+    .catch(err => {
+        console.log(err);
+    });
+})
+
 app.get('/blogs/create', (req, res) => {
     res.render('create', {title: 'Create a new blog'});
 });
